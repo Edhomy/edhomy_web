cc7913ecee29762bd27d434503b157ee725d95b4
Cristian A. Jara Gutierrez <cristian.jara@edhomy.com>
2025-11-22 02:28:05 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1291726703"
+    serviceWorkerVersion: "1406384069"
   }
 });
