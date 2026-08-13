322b0c38e7f83b004e9614c180973e66ab28c379
Cristian from Edhomy <cristian.jara@edhomy.com>
2026-08-12 20:13:05 -0400

    Add files via upload

--- main.dart.js
+++ main.dart.js
@@ -29016,7 +29016,7 @@ $S:2}
 A.aCJ.prototype={
 $1(a){var s=A.dr().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:30}
 A.KS.prototype={
 Yj(){var s=this.aeH(),r=$.b5.bf().ImageFilter.MakeColorFilter(s,null)
